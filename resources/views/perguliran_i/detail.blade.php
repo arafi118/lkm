380af9d24ba94b8b09f242ca7cb97ed3f807d775
nurfandi31 <fandinur241@gmail.com>
2025-03-04 13:21:03 +0700

    jaminan aktif

--- resources/views/perguliran_i/detail.blade.php
+++ resources/views/perguliran_i/detail.blade.php
@@ -166,9 +166,15 @@
             'withExcel' => false,
         ];
     }
+    // $decoded = json_decode($perguliran_i->jaminan, true);
+    // $jenis_jaminan =
+    //     strlen($perguliran_i->jaminan) > 6 && is_array($decoded) && isset($decoded['jenis_jaminan'])
+    //         ? $decoded['jenis_jaminan']
+    //         : '0';
 
     $jenis_jaminan =
         strlen($perguliran_i->jaminan) > 6 ? json_decode($perguliran_i->jaminan, true)['jenis_jaminan'] : '0';
+
 @endphp
 @extends('layouts.base')
 
@@ -207,7 +213,7 @@
                             <i class="fa fa-trash"></i>&nbsp; HAPUS PROPOSAL
                         </button>
                     </div>
-                @elseif ($perguliran_i->status == 'W')
+                @elseif(in_array($perguliran_i->status, ['A', 'W']))
                     <div class="page-title-actions">
                         <button type="button" data-bs-toggle="tooltip" data-bs-placement="bottom"
                             class="btn-shadow me-3 btn btn-success" id="BtnEditWaitingList">
