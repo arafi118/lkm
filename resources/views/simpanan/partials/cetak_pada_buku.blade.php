4ebecf4c16a52b37bab8b7fdf1f51c4b5f37eaa0
yukaristel <rokumitsu012@gmail.com>
2025-04-15 08:40:29 +0700

    pada buku

--- resources/views/simpanan/partials/cetak_pada_buku.blade.php
+++ resources/views/simpanan/partials/cetak_pada_buku.blade.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <style>
-    .style6 { font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 16px; color: #000000; }
-.style9 { font-family: Arial, Helvetica, sans-serif; font-size: 10px; color: #000000; }
-.style10 { font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 10px; color: #000000; }
-.style26 { font-family: Verdana, Arial, Helvetica, sans-serif; color: #000000; }
-.style27 { font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 11px; font-weight: bold; color: #000000; }
 
-        .top {border-top: 1px solid #000000; }
-        .bottom {border-bottom: 1px solid #000000; }
-        .left {border-left: 1px solid #000000; }
-        .right {border-right: 1px solid #000000; }
-        .all {border: 1px solid #000000; }
-        .align-justify {text-align:justify; }
-        .align-center {text-align:center; }
-        .align-right {text-align:right; }
-    </style>
+<style type="text/css">
+<!--
+.style6 {font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 16px;  }
+.style9 {font-family:  Arial, Helvetica, sans-serif; font-size: 10px; }
+.style10 {font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 10px; }
+.top	{border-top: 1px solid #000000; }
+.bottom	{border-bottom: 1px solid #000000; }
+.left	{border-left: 1px solid #000000; }
+.right	{border-right: 1px solid #000000; }
+.all	{border: 1px solid #000000; }
+.style26 {font-family: Verdana, Arial, Helvetica, sans-serif}
+.style27 {font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 11px; font-weight: bold; }
+.align-justify {text-align:justify; }
+.align-center {text-align:center; }
+.align-right {text-align:right; }
+-->
+</style>
+
     <title>{{ $title }}</title>
 </head>
 <body onload="window.print()">
@@ -45,13 +48,14 @@
         @endfor
 
         <tr>
-            <td width="5%" class="style9 align-center">{{ $br }}</td>
+            <td width="5%" class="style9 align-justify">{{ $br }}</td>
             <td width="12%" class="style9 align-center">{{ $transaksi->tgl_transaksi }}</td>
             <td width="5%" class="style9 align-center">{{ $transaksi->realSimpanan->kode }}</td>
-            <td width="23%" class="style9 align-center">{{ number_format($transaksi->realSimpanan->real_d) }}</td>
-            <td width="23%" class="style9 align-center">{{ number_format($transaksi->realSimpanan->real_k) }}</td>
-            <td width="23%" class="style9 align-center">{{ number_format($transaksi->realSimpanan->sum) }}</td>
-            <td width="9%" class="style9 align-center">{{ strtoupper($user) }}-{{ $transaksi->idt }}</td>
+            <td width="21%" class="style9 align-center">{{ number_format($transaksi->realSimpanan->real_d) }}</td>
+            <td width="21%" class="style9 align-center">{{ number_format($transaksi->realSimpanan->real_k) }}</td>
+            <td width="21%" class="style9 align-center">{{ number_format($transaksi->realSimpanan->sum) }}</td>
+            <td width="10%" class="style9 align-center">{{ strtoupper($user) }}-{{ $transaksi->idt }}</td>
+			<td width="5%" class="style9 align-center" >&nbsp;</td>
         </tr>
     </table>
 </body>
