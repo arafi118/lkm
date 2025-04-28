86ac5f0bf6fe0913075da2e97a99e2df8a989ec5
yukaristel <rokumitsu012@gmail.com>
2025-04-28 10:42:44 +0700

    coba

--- routes/web.php
+++ routes/web.php
@@ -125,10 +125,10 @@ Route::get('/app', [AuthController::class, 'app']);
 
 // Route::get('/force/{uname}', [AuthController::class, 'force'])->middleware('guest');
 
-Route::get('/pelaporan', [PelaporanController::class, 'index'])->middleware('basic', 'is_aktif');
-Route::get('/pelaporan/sub_laporan/{file}', [PelaporanController::class, 'subLaporan'])->middleware('basic', 'is_aktif');
-Route::post('/pelaporan/preview', [PelaporanController::class, 'preview'])->middleware('basic', 'is_aktif');
-Route::post('/pelaporan/preview/{lokasi?}', [PelaporanController::class, 'preview'])->middleware('basic', 'is_aktif');
+Route::get('/pelaporan', [PelaporanController::class, 'index'])->middleware('auth', 'is_aktif');
+Route::get('/pelaporan/sub_laporan/{file}', [PelaporanController::class, 'subLaporan'])->middleware('auth', 'is_aktif');
+Route::post('/pelaporan/preview', [PelaporanController::class, 'preview'])->middleware('auth', 'is_aktif');
+Route::post('/pelaporan/preview/{lokasi?}', [PelaporanController::class, 'preview'])->middleware('auth', 'is_aktif');
 
 Route::get('/pelaporan/mou', [PelaporanController::class, 'mou'])->middleware('auth', 'is_aktif');
 Route::get('/pelaporan/ts', [PelaporanController::class, 'ts'])->middleware('auth', 'is_aktif');
