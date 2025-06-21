5b5b09ef7490845d0ab95dc2ee5f9556397d4557
arafi118 <arp46301@gmail.com>
2025-06-21 11:39:43 +0700

    Excel

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -3,13 +3,34 @@
 namespace App\Http\Controllers;
 
 use App\Imports\ExcelImport;
+use App\Models\AkunLevel2;
+use App\Models\Rekening;
+use App\Utils\Keuangan;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Excel as ExcelExcel;
+use Session;
 
 class ExcelController extends Controller
 {
     public function index()
     {
+        $akun_level_2 = [];
+        $akun_level_3 = [];
+        $akun_rekening = [];
+
+        Session::put('lokasi', '351');
+        $rekening = Rekening::all();
+        foreach ($rekening as $rek) {
+            $kode_akun = explode('.', $rek->kode_akun);
+            $kode_akun_level_2 = $kode_akun[0] . '.' . $kode_akun[1];
+            $kode_akun_level_3 = $kode_akun[0] . '.' . $kode_akun[1] . '.' . str_pad($kode_akun[2], 2, '0', STR_PAD_LEFT);
+            $kode_akun_rekening = $rek->kode_akun;
+
+            $akun_level_2[$kode_akun_level_2][] = $rek;
+            $akun_level_3[$kode_akun_level_3][] = $rek;
+            $akun_rekening[$kode_akun_rekening][] = $rek;
+        }
+
         $index = 0;
         $insert = [];
         $excel = (new ExcelImport)->toArray('storage/excel/arus-kas-arthamari.xlsx', null, ExcelExcel::XLSX);
@@ -64,6 +85,8 @@ class ExcelController extends Controller
         $nomor = 1;
         $grand_parent_id = 0;
         $sub_parent_id = 0;
+        $kode_debit = [];
+        $kode_kredit = [];
         $aruskas = [];
         foreach ($insert as $data) {
             if ($data['number']) {
@@ -98,15 +121,54 @@ class ExcelController extends Controller
             if (count($data['child']) > 0) {
                 $child = $data['child'];
                 foreach ($child as $key => $value) {
-                    $aruskas[] = [
-                        'id' => $nomor,
-                        'nama_akun' => 'NULL',
-                        'parent_id' => $child_parent_id,
-                        'debit' => $value['debit'],
-                        'kredit' => $value['kredit'],
-                        'aktif' => 'Y'
-                    ];
-                    $nomor++;
+                    $fixDebit = true;
+                    $kode_fixed = $value['debit'];
+                    $kode_loop = $value['kredit'];
+                    if (Keuangan::startWith($value['kredit'], '1.1.01')) {
+                        $fixDebit = false;
+                        $kode_fixed = $value['kredit'];
+                        $kode_loop = $value['debit'];
+                    }
+
+                    if (strlen($kode_loop) == '3') {
+                        $loop = $akun_level_2[$kode_loop];
+                    }
+
+                    if (strlen($kode_loop) == '6') {
+                        $loop = $akun_level_3[$kode_loop];
+                    }
+
+                    if (strlen($kode_loop) == '9') {
+                        $loop = $akun_rekening[$kode_loop];
+                    }
+
+                    foreach ($loop as $rek) {
+
+
+                        if ($fixDebit) {
+                            if (in_array($rek->kode_akun, $kode_debit)) {
+                                continue;
+                            }
+                            $kode_debit[] = $rek->kode_akun;
+                        } else {
+                            if (in_array($rek->kode_akun, $kode_kredit)) {
+                                continue;
+                            }
+                            $kode_kredit[] = $rek->kode_akun;
+                        }
+
+                        $aruskas[] = [
+                            'id' => $nomor,
+                            'nama_akun' => 'NULL',
+                            'parent_id' => $child_parent_id,
+                            'debit' => ($fixDebit) ? $kode_fixed : $rek->kode_akun,
+                            'kredit' => ($fixDebit) ? $rek->kode_akun : $kode_fixed,
+                            'aktif' => 'Y'
+                        ];
+
+                        $kode_akun[] = $rek->kode_akun;
+                        $nomor++;
+                    }
                 }
             }
         }
