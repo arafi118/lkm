89887b34b82619adeace9f71550d9c31417951a2
yukaristel <rokumitsu012@gmail.com>
2025-10-08 13:37:35 +0700

    arus kas builder (Y/N)

--- app/Models/ArusKasLkm.php
+++ app/Models/ArusKasLkm.php
@@ -4,22 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class ArusKasLkm extends Model
 {
     use HasFactory;
 
+    protected static function booted()
+    {
+        static::addGlobalScope('aktif', function (Builder $builder) {
+            $builder->where('status', 'Y');
+        });
+    }
+
     public function getTable()
     {
         $lokasi = session('lokasi');
-
         $lokasiKop = [1, 351, 352, 353, 354];
 
-        if (in_array($lokasi, $lokasiKop)) {
-            return 'arus_kas_kop';
-        }
-
-        return 'arus_kas_lkm';
+        return in_array($lokasi, $lokasiKop)
+            ? 'arus_kas_kop'
+            : 'arus_kas_lkm';
     }
 
     public function child()
