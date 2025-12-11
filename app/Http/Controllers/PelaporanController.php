95962676137362df15a95e153b3f2dcd434c14e7
Yukari <Yukari@FSOS-FO7JLB4A57>
2025-12-11 14:01:28 +0700

    kolek desa

--- app/Http/Controllers/PelaporanController.php
+++ app/Http/Controllers/PelaporanController.php
@@ -2886,70 +2886,69 @@ class PelaporanController extends Controller
             $data['tgl'] = Tanggal::namaBulan($tgl) . ' ' . Tanggal::tahun($tgl);
         }
 
-        $data['jenis_pp'] = JenisProdukPinjaman::where(function ($query) {
+        $kec = Kecamatan::where('id', Session::get('lokasi'))->first();
+        $data['jenis_pp'] = JenisProdukPinjaman::where(function ($query) use ($kec) {
             $query->where('lokasi', '0')
-                ->where('kecuali', 'NOT LIKE', '%#' . session('lokasi') . '#%');
-        })
-            ->orWhere(function ($query) {
-                $query->where('lokasi', session('lokasi'))
-                    ->where('kecuali', 'NOT LIKE', '%#' . session('lokasi') . '#%');
-            })
-            ->with([
-                'pinjaman_anggota' => function ($query) use ($data) {
-                    $tb_pinj_i = 'pinjaman_anggota_' . $data['kec']->id;
-                    $tb_ang = 'anggota_' . $data['kec']->id;
-                    $data['tb_pinj_i'] = $tb_pinj_i;
+                ->orWhere(function ($query) use ($kec) {
+                    $query->where('kecuali', 'NOT LIKE', "%-{$kec['id']}-%")
+                        ->where('lokasi', 'LIKE', "%-{$kec['id']}-%");
+                });
+        })->with([
+            'pinjaman_anggota' => function ($query) use ($data) {
+                $tb_pinj_i = 'pinjaman_anggota_' . $data['kec']->id;
+                $tb_ang = 'anggota_' . $data['kec']->id;
+                $data['tb_pinj_i'] = $tb_pinj_i;
 
-                    $query->select($tb_pinj_i . '.*', $tb_ang . '.namadepan', 'desa.nama_desa', 'desa.kd_desa', 'desa.kode_desa', 'sebutan_desa.sebutan_desa')
-                        ->join($tb_ang, $tb_ang . '.id', '=', $tb_pinj_i . '.nia')
-                        ->join('desa', $tb_ang . '.desa', '=', 'desa.kd_desa')
-                        ->join('sebutan_desa', 'sebutan_desa.id', '=', 'desa.sebutan')
-                        ->withSum(['real_i' => function ($query) use ($data) {
-                            $query->where('tgl_transaksi', 'LIKE', '%' . $data['tahun'] . '-' . $data['bulan'] . '-%');
-                        }], 'realisasi_pokok')
-                        ->withSum(['real_i' => function ($query) use ($data) {
-                            $query->where('tgl_transaksi', 'LIKE', '%' . $data['tahun'] . '-' . $data['bulan'] . '-%');
-                        }], 'realisasi_jasa')
-                        ->where($tb_pinj_i . '.sistem_angsuran', '!=', '12')->where(function ($query) use ($data) {
-                            $query->where([
-                                [$data['tb_pinj_i'] . '.status', 'A'],
-                                [$data['tb_pinj_i'] . '.tgl_cair', '<=', $data['tgl_kondisi']]
-                            ])->orwhere([
-                                [$data['tb_pinj_i'] . '.status', 'L'],
-                                [$data['tb_pinj_i'] . '.tgl_cair', '<=', $data['tgl_kondisi']],
-                                [$data['tb_pinj_i'] . '.tgl_lunas', '>=', "$data[tahun]-01-01"]
-                            ])->orwhere([
-                                [$data['tb_pinj_i'] . '.status', 'L'],
-                                [$data['tb_pinj_i'] . '.tgl_lunas', '<=', $data['tgl_kondisi']],
-                                [$data['tb_pinj_i'] . '.tgl_lunas', '>=', "$data[tahun]-01-01"]
-                            ])->orwhere([
-                                [$data['tb_pinj_i'] . '.status', 'R'],
-                                [$data['tb_pinj_i'] . '.tgl_cair', '<=', $data['tgl_kondisi']],
-                                [$data['tb_pinj_i'] . '.tgl_lunas', '>=', "$data[tahun]-01-01"]
-                            ])->orwhere([
-                                [$data['tb_pinj_i'] . '.status', 'R'],
-                                [$data['tb_pinj_i'] . '.tgl_lunas', '<=', $data['tgl_kondisi']],
-                                [$data['tb_pinj_i'] . '.tgl_lunas', '>=', "$data[tahun]-01-01"]
-                            ])->orwhere([
-                                [$data['tb_pinj_i'] . '.status', 'H'],
-                                [$data['tb_pinj_i'] . '.tgl_cair', '<=', $data['tgl_kondisi']],
-                                [$data['tb_pinj_i'] . '.tgl_lunas', '>=', "$data[tahun]-01-01"]
-                            ])->orwhere([
-                                [$data['tb_pinj_i'] . '.status', 'H'],
-                                [$data['tb_pinj_i'] . '.tgl_lunas', '<=', $data['tgl_kondisi']],
-                                [$data['tb_pinj_i'] . '.tgl_lunas', '>=', "$data[tahun]-01-01"]
-                            ]);
-                        })
-                        ->orderBy($tb_ang . '.desa', 'ASC')
-                        ->orderBy($tb_pinj_i . '.tgl_cair', 'ASC');
-                },
-                'pinjaman_anggota.saldo' => function ($query) use ($data) {
-                    $query->where('tgl_transaksi', '<=', $data['tgl_kondisi']);
-                },
-                'pinjaman_anggota.target' => function ($query) use ($data) {
-                    $query->where('jatuh_tempo', '<=', $data['tgl_kondisi']);
-                }
-            ])->get();
+                $query->select($tb_pinj_i . '.*', $tb_ang . '.namadepan', 'desa.nama_desa', 'desa.kd_desa', 'desa.kode_desa', 'sebutan_desa.sebutan_desa')
+                    ->join($tb_ang, $tb_ang . '.id', '=', $tb_pinj_i . '.nia')
+                    ->join('desa', $tb_ang . '.desa', '=', 'desa.kd_desa')
+                    ->join('sebutan_desa', 'sebutan_desa.id', '=', 'desa.sebutan')
+                    ->withSum(['real_i' => function ($query) use ($data) {
+                        $query->where('tgl_transaksi', 'LIKE', '%' . $data['tahun'] . '-' . $data['bulan'] . '-%');
+                    }], 'realisasi_pokok')
+                    ->withSum(['real_i' => function ($query) use ($data) {
+                        $query->where('tgl_transaksi', 'LIKE', '%' . $data['tahun'] . '-' . $data['bulan'] . '-%');
+                    }], 'realisasi_jasa')
+                    ->where($tb_pinj_i . '.sistem_angsuran', '!=', '12')->where(function ($query) use ($data) {
+                        $query->where([
+                            [$data['tb_pinj_i'] . '.status', 'A'],
+                            [$data['tb_pinj_i'] . '.tgl_cair', '<=', $data['tgl_kondisi']]
+                        ])->orwhere([
+                            [$data['tb_pinj_i'] . '.status', 'L'],
+                            [$data['tb_pinj_i'] . '.tgl_cair', '<=', $data['tgl_kondisi']],
+                            [$data['tb_pinj_i'] . '.tgl_lunas', '>=', "$data[tahun]-01-01"]
+                        ])->orwhere([
+                            [$data['tb_pinj_i'] . '.status', 'L'],
+                            [$data['tb_pinj_i'] . '.tgl_lunas', '<=', $data['tgl_kondisi']],
+                            [$data['tb_pinj_i'] . '.tgl_lunas', '>=', "$data[tahun]-01-01"]
+                        ])->orwhere([
+                            [$data['tb_pinj_i'] . '.status', 'R'],
+                            [$data['tb_pinj_i'] . '.tgl_cair', '<=', $data['tgl_kondisi']],
+                            [$data['tb_pinj_i'] . '.tgl_lunas', '>=', "$data[tahun]-01-01"]
+                        ])->orwhere([
+                            [$data['tb_pinj_i'] . '.status', 'R'],
+                            [$data['tb_pinj_i'] . '.tgl_lunas', '<=', $data['tgl_kondisi']],
+                            [$data['tb_pinj_i'] . '.tgl_lunas', '>=', "$data[tahun]-01-01"]
+                        ])->orwhere([
+                            [$data['tb_pinj_i'] . '.status', 'H'],
+                            [$data['tb_pinj_i'] . '.tgl_cair', '<=', $data['tgl_kondisi']],
+                            [$data['tb_pinj_i'] . '.tgl_lunas', '>=', "$data[tahun]-01-01"]
+                        ])->orwhere([
+                            [$data['tb_pinj_i'] . '.status', 'H'],
+                            [$data['tb_pinj_i'] . '.tgl_lunas', '<=', $data['tgl_kondisi']],
+                            [$data['tb_pinj_i'] . '.tgl_lunas', '>=', "$data[tahun]-01-01"]
+                        ]);
+                    })
+                    ->orderBy($tb_ang . '.desa', 'ASC')
+                    ->orderBy($tb_pinj_i . '.tgl_cair', 'ASC');
+            },
+            'pinjaman_anggota.saldo' => function ($query) use ($data) {
+                $query->where('tgl_transaksi', '<=', $data['tgl_kondisi']);
+            },
+            'pinjaman_anggota.target' => function ($query) use ($data) {
+                $query->where('jatuh_tempo', '<=', $data['tgl_kondisi']);
+            }
+        ])->get();
 
         $view = view('pelaporan.view.perkembangan_piutang.kolek_desa', $data)->render();
 
@@ -2960,6 +2959,7 @@ class PelaporanController extends Controller
             return $view;
         }
     }
+
     private function kredit_barang(array $data)
     {
         $thn = $data['tahun'];
--- resources/views/pelaporan/view/perkembangan_piutang/kolek_desa.blade.php
+++ resources/views/pelaporan/view/perkembangan_piutang/kolek_desa.blade.php
@@ -2,6 +2,53 @@
     use App\Utils\Tanggal;
     $section = 0;
     $nomor_jenis_pp = 0;
+    
+    // Ambil data kolek dari database
+    $kolekData = $kec->kolek ? json_decode($kec->kolek, true) : [];
+    
+    // Filter hanya kolek yang aktif (ada nama)
+    $activeKolek = array_filter($kolekData, function($k) {
+        return !empty($k['nama']);
+    });
+    
+    // Fungsi untuk menentukan tingkat kolek
+    function getTingkatKolek($kolek_bulan, $kolekData) {
+        if (empty($kolekData)) {
+            return 0;
+        }
+        
+        // Loop dari tingkat kolek terendah ke tertinggi
+        for ($i = 0; $i < count($kolekData); $i++) {
+            $kolek = $kolekData[$i];
+            
+            // Skip jika kolek tidak aktif
+            if (empty($kolek['nama'])) {
+                continue;
+            }
+            
+            $durasi = floatval($kolek['durasi']);
+            $satuan = $kolek['satuan'];
+            
+            // Konversi durasi ke bulan jika satuan hari
+            if ($satuan == 'hari') {
+                $durasi = $durasi / 30;
+            }
+            
+            // Jika kolek_bulan kurang dari durasi, maka masuk ke tingkat ini
+            if ($kolek_bulan < $durasi) {
+                return $i;
+            }
+        }
+        
+        // Jika melebihi semua durasi, masuk ke tingkat kolek tertinggi
+        for ($i = count($kolekData) - 1; $i >= 0; $i--) {
+            if (!empty($kolekData[$i]['nama'])) {
+                return $i;
+            }
+        }
+        
+        return 0;
+    }
 @endphp
 
 @extends('pelaporan.layout.base')
@@ -21,9 +68,9 @@
             $t_saldo = 0;
             $t_tunggakan_pokok = 0;
             $t_tunggakan_jasa = 0;
-            $t_kolek1 = 0;
-            $t_kolek2 = 0;
-            $t_kolek3 = 0;
+            
+            // Inisialisasi total kolek secara dinamis
+            $t_kolek = array_fill(0, count($kolekData), 0);
         @endphp
         @if ($nomor_jenis_pp != 0)
             <div class="break"></div>
@@ -55,16 +102,46 @@
                 <th class="t l b" rowspan="2" width="10%">Saldo</th>
                 <th class="t l b" rowspan="2" width="4%">%</th>
                 <th class="t l b" colspan="2" width="20%">Tunggakan</th>
-                <th class="t l b" width="10%">Lancar</th>
-                <th class="t l b" width="10%">Diragukan</th>
-                <th class="t l b r" width="10%">Macet</th>
+                @foreach ($activeKolek as $index => $kolek)
+                    <th class="t l b {{ $loop->last ? 'r' : '' }}">{{ strtoupper($kolek['nama']) }}</th>
+                @endforeach
             </tr>
             <tr>
                 <th class="t l b" width="10%">Pokok</th>
                 <th class="t l b" width="10%">Jasa</th>
-                <th class="t l b">(Menunggak 0-6)</th>
-                <th class="t l b">(Menunggak 6+ s/d 12)</th>
-                <th class="t l b r">(Menunggak 12+)</th>
+                @foreach ($activeKolek as $index => $kolek)
+                    @php
+                        $durasi = $kolek['durasi'];
+                        $satuan = $kolek['satuan'];
+                        
+                        // Ambil durasi kolek sebelumnya untuk range
+                        $durasi_sebelum = 0;
+                        if ($index > 0) {
+                            $kolek_sebelum = $kolekData[$index - 1];
+                            $durasi_sebelum = $kolek_sebelum['durasi'];
+                            if ($kolek_sebelum['satuan'] == 'hari') {
+                                $durasi_sebelum = round($durasi_sebelum / 30, 1);
+                            }
+                        }
+                        
+                        // Format durasi untuk tampilan
+                        if ($satuan == 'hari') {
+                            $durasi_tampil = round($durasi / 30, 1);
+                        } else {
+                            $durasi_tampil = $durasi;
+                        }
+                        
+                        // Buat label range
+                        if ($index == 0) {
+                            $label = "(0 s/d {$durasi_tampil})";
+                        } elseif ($loop->last) {
+                            $label = "({$durasi_sebelum}+)";
+                        } else {
+                            $label = "({$durasi_sebelum} s/d {$durasi_tampil})";
+                        }
+                    @endphp
+                    <th class="t l b {{ $loop->last ? 'r' : '' }}">{{ $label }}</th>
+                @endforeach
             </tr>
 
             @foreach ($jpp->pinjaman_anggota as $pinkel)
@@ -81,9 +158,10 @@
                             $t_saldo += $j_saldo;
                             $t_tunggakan_pokok += $j_tunggakan_pokok;
                             $t_tunggakan_jasa += $j_tunggakan_jasa;
-                            $t_kolek1 += $j_kolek1;
-                            $t_kolek2 += $j_kolek2;
-                            $t_kolek3 += $j_kolek3;
+                            
+                            foreach ($j_kolek as $idx => $val) {
+                                $t_kolek[$idx] += $val;
+                            }
                         @endphp
                         <tr>
                             <td class="t l b" align="left">{{ $nomor++ }}. {{ $nama_desa }}</td>
@@ -92,9 +170,11 @@
                             <td class="t l b" align="center">{{ number_format(floor($j_pross * 100)) }}</td>
                             <td class="t l b" align="right">{{ number_format($j_tunggakan_pokok) }}</td>
                             <td class="t l b" align="right">{{ number_format($j_tunggakan_jasa) }}</td>
-                            <td class="t l b" align="right">{{ number_format($j_kolek1) }}</td>
-                            <td class="t l b" align="right">{{ number_format($j_kolek2) }}</td>
-                            <td class="t l b r" align="right">{{ number_format($j_kolek3) }}</td>
+                            @foreach ($activeKolek as $idx => $kolek)
+                                <td class="t l b {{ $loop->last ? 'r' : '' }}" align="right">
+                                    {{ number_format($j_kolek[$idx] ?? 0) }}
+                                </td>
+                            @endforeach
                         </tr>
                     @endif
 
@@ -103,9 +183,7 @@
                         $j_saldo = 0;
                         $j_tunggakan_pokok = 0;
                         $j_tunggakan_jasa = 0;
-                        $j_kolek1 = 0;
-                        $j_kolek2 = 0;
-                        $j_kolek3 = 0;
+                        $j_kolek = array_fill(0, count($kolekData), 0);
                         $section = $pinkel->kd_desa;
                         $nama_desa = $pinkel->sebutan_desa . ' ' . $pinkel->nama_desa;
                     @endphp
@@ -155,17 +233,7 @@
 
                     $pross = $saldo_pokok == 0 ? 0 : $saldo_pokok / $pinkel->alokasi;
 
-                    if ($pinkel->tgl_lunas <= $tgl_kondisi && $pinkel->status == 'L') {
-                        $tunggakan_pokok = 0;
-                        $tunggakan_jasa = 0;
-                        $saldo_pokok = 0;
-                        $saldo_jasa = 0;
-                    } elseif ($pinkel->tgl_lunas <= $tgl_kondisi && $pinkel->status == 'R') {
-                        $tunggakan_pokok = 0;
-                        $tunggakan_jasa = 0;
-                        $saldo_pokok = 0;
-                        $saldo_jasa = 0;
-                    } elseif ($pinkel->tgl_lunas <= $tgl_kondisi && $pinkel->status == 'H') {
+                    if ($pinkel->tgl_lunas <= $tgl_kondisi && in_array($pinkel->status, ['L', 'R', 'H'])) {
                         $tunggakan_pokok = 0;
                         $tunggakan_jasa = 0;
                         $saldo_pokok = 0;
@@ -175,41 +243,30 @@
                     $tgl_akhir = new DateTime($tgl_kondisi);
                     $tgl_awal = new DateTime($pinkel->tgl_cair);
                     $selisih = $tgl_akhir->diff($tgl_awal);
-
-                    // if ($lpp == 'Minggu') {
-                    //     $selisih_hari = $selisih->days;
-                    //     $selisih = floor($selisih_hari / 7);
-                    // } else {
-                        
-                    // }
                     $selisih = $selisih->y * 12 + $selisih->m;
 
                     $_kolek = 0;
                     if ($wajib_pokok != '0') {
                         $_kolek = $tunggakan_pokok / $wajib_pokok;
                     }
-                    $kolek = round($_kolek + ($selisih - $angsuran_ke));
-                    if ($kolek < 6) {
-                        $kolek1 = $saldo_pokok;
-                        $kolek2 = 0;
-                        $kolek3 = 0;
-                    } elseif ($kolek >= 6 && $kolek <= 12) {
-                        $kolek1 = 0;
-                        $kolek2 = $saldo_pokok;
-                        $kolek3 = 0;
-                    } else {
-                        $kolek1 = 0;
-                        $kolek2 = 0;
-                        $kolek3 = $saldo_pokok;
-                    }
+                    
+                    $kolek_bulan = round($_kolek + ($selisih - $angsuran_ke));
+                    
+                    // Tentukan tingkat kolek berdasarkan konfigurasi database
+                    $tingkat_kolek = getTingkatKolek($kolek_bulan, $kolekData);
+                    
+                    // Inisialisasi array kolek untuk baris ini
+                    $row_kolek = array_fill(0, count($kolekData), 0);
+                    $row_kolek[$tingkat_kolek] = $saldo_pokok;
 
                     $j_alokasi += $pinkel->alokasi;
                     $j_saldo += $saldo_pokok;
                     $j_tunggakan_pokok += $tunggakan_pokok;
                     $j_tunggakan_jasa += $tunggakan_jasa;
-                    $j_kolek1 += $kolek1;
-                    $j_kolek2 += $kolek2;
-                    $j_kolek3 += $kolek3;
+                    
+                    foreach ($row_kolek as $idx => $val) {
+                        $j_kolek[$idx] += $val;
+                    }
                 @endphp
             @endforeach
 
@@ -220,9 +277,10 @@
                     $t_saldo += $j_saldo;
                     $t_tunggakan_pokok += $j_tunggakan_pokok;
                     $t_tunggakan_jasa += $j_tunggakan_jasa;
-                    $t_kolek1 += $j_kolek1;
-                    $t_kolek2 += $j_kolek2;
-                    $t_kolek3 += $j_kolek3;
+                    
+                    foreach ($j_kolek as $idx => $val) {
+                        $t_kolek[$idx] += $val;
+                    }
                 @endphp
                 <tr>
                     <td class="t l b" align="left">{{ $nomor++ }}. {{ $nama_desa }}</td>
@@ -231,9 +289,11 @@
                     <td class="t l b" align="center">{{ number_format(floor($j_pross * 100)) }}</td>
                     <td class="t l b" align="right">{{ number_format($j_tunggakan_pokok) }}</td>
                     <td class="t l b" align="right">{{ number_format($j_tunggakan_jasa) }}</td>
-                    <td class="t l b" align="right">{{ number_format($j_kolek1) }}</td>
-                    <td class="t l b" align="right">{{ number_format($j_kolek2) }}</td>
-                    <td class="t l b r" align="right">{{ number_format($j_kolek3) }}</td>
+                    @foreach ($activeKolek as $idx => $kolek)
+                        <td class="t l b {{ $loop->last ? 'r' : '' }}" align="right">
+                            {{ number_format($j_kolek[$idx] ?? 0) }}
+                        </td>
+                    @endforeach
                 </tr>
 
                 @php
@@ -241,9 +301,16 @@
                     if ($t_saldo) {
                         $t_pross = $t_saldo / $t_alokasi;
                     }
+                    
+                    // Hitung total resiko pinjaman
+                    $total_resiko = 0;
+                    foreach ($activeKolek as $idx => $kolek) {
+                        $prosentase = floatval($kolek['prosentase']);
+                        $total_resiko += ($t_kolek[$idx] * $prosentase) / 100;
+                    }
                 @endphp
                 <tr>
-                    <td colspan="9" style="padding: 0px !important;">
+                    <td colspan="{{ 6 + count($activeKolek) }}" style="padding: 0px !important;">
                         <table class="p" border="0" width="100%" cellspacing="0" cellpadding="0"
                             style="font-size: 11px; table-layout: fixed;">
                             <tr style="font-weight: bold;">
@@ -256,28 +323,40 @@
                                 </td>
                                 <td class="t l b" width="10%" align="right">{{ number_format($t_tunggakan_jasa) }}
                                 </td>
-                                <td class="t l b" width="10%" align="right">{{ number_format($t_kolek1) }}</td>
-                                <td class="t l b" width="10%" align="right">{{ number_format($t_kolek2) }}</td>
-                                <td class="t l b r" width="10%" align="right">{{ number_format($t_kolek3) }}</td>
+                                @foreach ($activeKolek as $idx => $kolek)
+                                    <td class="t l b {{ $loop->last ? 'r' : '' }}" align="right">
+                                        {{ number_format($t_kolek[$idx] ?? 0) }}
+                                    </td>
+                                @endforeach
                             </tr>
                             <tr style="font-weight: bold;">
                                 <td class="t l b" align="center" rowspan="2" height="20">Resiko Pinjaman</td>
-                                <td class="t l b" colspan="5" align="center">(Lancar + Diragukan + Macet)</td>
-                                <td class="t l b" align="center">Lancar * 0%</td>
-                                <td class="t l b" align="center">Diragukan * 50%</td>
-                                <td class="t l b r" align="center">Macet * 100%</td>
+                                <td class="t l b" colspan="5" align="center">
+                                    ({{ implode(' + ', array_map(function($k) { return $k['nama']; }, $activeKolek)) }})
+                                </td>
+                                @foreach ($activeKolek as $idx => $kolek)
+                                    <td class="t l b {{ $loop->last ? 'r' : '' }}" align="center">
+                                        {{ $kolek['nama'] }} * {{ $kolek['prosentase'] }}%
+                                    </td>
+                                @endforeach
                             </tr>
                             <tr>
                                 <td class="t l b" align="center" colspan="5">
-                                    {{ number_format(($t_kolek1 * 0) / 100 + ($t_kolek2 * 50) / 100 + ($t_kolek3 * 100) / 100) }}
+                                    {{ number_format($total_resiko) }}
                                 </td>
-                                <td class="t l b" align="center">{{ number_format(($t_kolek1 * 0) / 100) }}</td>
-                                <td class="t l b" align="center">{{ number_format(($t_kolek2 * 50) / 100) }}</td>
-                                <td class="t l b r" align="center">{{ number_format(($t_kolek3 * 100) / 100) }}</td>
+                                @foreach ($activeKolek as $idx => $kolek)
+                                    @php
+                                        $prosentase = floatval($kolek['prosentase']);
+                                        $nilai_resiko = ($t_kolek[$idx] * $prosentase) / 100;
+                                    @endphp
+                                    <td class="t l b {{ $loop->last ? 'r' : '' }}" align="center">
+                                        {{ number_format($nilai_resiko) }}
+                                    </td>
+                                @endforeach
                             </tr>
 
                             <tr>
-                                <td colspan="9">
+                                <td colspan="{{ 6 + count($activeKolek) }}">
                                     <div style="margin-top: 16px;"></div>
                                     {!! json_decode(str_replace('{tanggal}', $tanggal_kondisi, $kec->ttd->tanda_tangan_pelaporan), true) !!}
                                 </td>
