055df1c8702ee13283d16935b25cb32372f5b4b4
Yukari <Yukari@FSOS-FO7JLB4A57>
2026-02-14 08:20:50 +0700

    login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,7 +54,7 @@
 <body class="bg-default">
     <div class="main-content">
         <!-- Header -->
-        <div class="header bg-gradient-primary py-7 py-lg-8">
+        <div class="header bg-gradient-primary py-6 py-lg-6">
             <div class="container">
                 <div class="header-body text-center mb-7">
                     <div class="row justify-content-center">
